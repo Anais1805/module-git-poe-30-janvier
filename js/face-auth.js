4e81e56fb8d7edb0eedfae6f3a24cb59c4d7e648
Anais1805 <111952504+Anais1805@users.noreply.github.com>
2023-01-31 14:09:39 +0100

    Add facebook auth
    If account already exists, log the user, otherwise create an account and log the user

---
+++ js/face-auth.js
@@ -0,0 +1,3 @@
+export function handleFacebookAuth(clientId:string):void{
+    console.log('hello')
+}
---
+++ js/validate-login.js
@@ -0,0 +1,5 @@
+const login = document.querySelector("#login");
+
+form.addEventListener("login", (e) => {
+    e.preventDefault();
+});
